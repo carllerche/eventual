05dbe85c6560eda2cc710e61a2418316ecb6d84a
Carl Lerche <me@carllerche.com>
2015-05-28 04:27:26 +0000

    rebuild pages at 33c8eec

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['eventual'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='eventual/struct.Timer.html' title='eventual::Timer'>Timer</a>",];
+implementors['eventual'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='eventual/struct.Timer.html' title='eventual::Timer'>Timer</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/marker/trait.Send.js
+++ implementors/core/marker/trait.Send.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['eventual'] = ["impl&lt;A: <a class='trait' href='eventual/trait.Async.html' title='eventual::Async'>Async</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='eventual/struct.Receipt.html' title='eventual::Receipt'>Receipt</a>&lt;A&gt;",];
+implementors['eventual'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;A: <a class='trait' href='eventual/trait.Async.html' title='eventual::Async'>Async</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='eventual/struct.Receipt.html' title='eventual::Receipt'>Receipt</a>&lt;A&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
