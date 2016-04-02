6e005e976556da83275987afd31b07d0e329cfd6
Carl Lerche <me@carllerche.com>
2016-04-02 04:47:01 +0000

    rebuild pages at c59bd86

--- implementors/core/iter/trait.Iterator.js
+++ implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['eventual'] = ["impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> + 'static, E: <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> + 'static&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='eventual/struct.StreamIter.html' title='eventual::StreamIter'>StreamIter</a>&lt;T, E&gt;",];
+implementors['eventual'] = ["impl&lt;T: <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> + 'static, E: <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> + 'static&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='eventual/struct.StreamIter.html' title='eventual::StreamIter'>StreamIter</a>&lt;T, E&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
